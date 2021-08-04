dbe556d173f7d38b83a0d7854268248820b7f0bd
MASHOD0 <mashhood2002@gmail.com>
2021-08-04 23:27:54 +0530

    init

---
+++ Paddle.lua
@@ -0,0 +1,14 @@
+--[[
+    This is CS50 2019.
+    Games Track
+    Pong
+
+    -- Paddle Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents a paddle that can move up and down. Used in the main
+    program to deflect the ball back toward the opponent.
+]]
+
