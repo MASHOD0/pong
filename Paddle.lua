a65b5814736b65831ef606e9fcdea52e8d7460bd
MASHOD0 <mashhood2002@gmail.com>
2021-08-04 23:28:07 +0530

    paddle classinit

--- Paddle.lua
+++ Paddle.lua
@@ -12,3 +12,25 @@
     program to deflect the ball back toward the opponent.
 ]]
 
+Paddle = Class{}
+
+--[[
+    The `init` function on our class is called just once, when the object
+    is first created. Used to set up all variables in the class and get it
+    ready for use.
+
+    Our Paddle should take an X and a Y, for positioning, as well as a width
+    and height for its dimensions.
+
+    Note that `self` is a reference to *this* object, whichever object is
+    instantiated at the time this function is called. Different objects can
+    have their own x, y, width, and height values, thus serving as containers
+    for data. In this sense, they're very similar to structs in C.
+]]
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dy = 0
+end
