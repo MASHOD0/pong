a8ea2194fcab0885c040316d30b8473e667b9d2f
MASHOD0 <mashhood2002@gmail.com>
2021-08-04 23:17:04 +0530

    displayFPS function

--- main.lua
+++ main.lua
@@ -314,3 +314,13 @@ function love.draw()
 
     push:apply('end')
 end
+--[[
+    Renders the current FPS.
+]]
+function displayFPS()
+    -- simple FPS display across all states
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+end
+
