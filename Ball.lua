0bdb6126c58ed96c905e1f95e92e5b68fdd400f9
MASHOD0 <mashhood2002@gmail.com>
2021-08-04 23:20:02 +0530

    init

---
+++ Ball.lua
@@ -0,0 +1,14 @@
+--[[
+    This is CS50 2019.
+    Games Track
+    Pong
+
+    -- Ball Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents a ball which will bounce back and forth between paddles
+    and walls until it passes a left or right boundary of the screen,
+    scoring a point for the opponent.
+]]
