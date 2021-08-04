1615f946d678eda0007b2c4076579c34b5883640
MASHOD0 <mashhood2002@gmail.com>
2021-08-04 23:20:35 +0530

    class ball init

--- Ball.lua
+++ Ball.lua
@@ -12,3 +12,16 @@
     and walls until it passes a left or right boundary of the screen,
     scoring a point for the opponent.
 ]]
+Ball = Class{}
+
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    -- these variables are for keeping track of our velocity on both the
+    -- X and Y axis, since the ball can move in two dimensions
+    self.dy = math.random(2) == 1 and -100 or 100
+    self.dx = math.random(2) == 1 and math.random(-80, -100) or math.random(80, 100)
+end
